95bab65492133cbfd76e21a1e65597ec3859f661
ingstadshakthi <ingstad26@gmail.com>
2026-06-17 23:11:12 +0530

    feat(): frontend app with reverse proxy

--- kubernetes/03. network/tasks-api/tasks-app.js
+++ kubernetes/03. network/tasks-api/tasks-app.js
@@ -11,6 +11,13 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  next();
+})
+
 // Ensure tasks file and directory exist
 const tasksDir = path.dirname(filePath);
 if (!fs.existsSync(tasksDir)) {
